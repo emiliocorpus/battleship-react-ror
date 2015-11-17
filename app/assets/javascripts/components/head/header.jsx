77492221d6b5629d4d3e833563741d938c2f0788
Emilio Corpus <emiliocorpus@gmail.com>
2015-11-17 14:12:43 -0800

    adds React components for header and body

---
+++ app/assets/javascripts/components/head/header.jsx
@@ -0,0 +1,9 @@
+var Header = React.createClass({
+	render:function() {
+		return (
+			<div id="header">
+				<span id="battleship">BATTLESHIP</span> <span id="description">built with Ruby on Rails and React.js</span> 
+			</div>
+		)
+	}
+})
---
+++ app/assets/javascripts/components/page.jsx
@@ -0,0 +1,10 @@
+var Page = React.createClass({
+	render:function(){
+		return (
+			<div id="page-container">
+				<Header />
+				<Body />
+			</div>
+		)
+	}
+})
